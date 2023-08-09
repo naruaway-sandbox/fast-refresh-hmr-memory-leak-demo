2fcdf0743c0d061f8c47ea50ff915a2529cebf34
Naru <naru@naruaway.com>
2023-08-09 20:45:45 +0900

    commit

--- rewrite.js
+++ rewrite.js
@@ -7,13 +7,15 @@ const targetFilePaths = [
   "./src/pages/App.jsx",
 ];
 
-const contents = fs.readFileSync(targetFilePath, "utf-8");
-
+// Just edit target files 1000 times with 100ms intervals
 for (let i = 0; i < 1000; ++i) {
   await sleep(100);
-  fs.writeFileSync(
-    targetFilePath,
-    contents.replace(/REWRITE_HERE\d*/, "REWRITE_HERE" + i),
-  );
+  for (const filePath of targetFilePaths) {
+    const contents = fs.readFileSync(filePath, "utf-8");
+    fs.writeFileSync(
+      filePath,
+      contents.replace(/REWRITE_HERE\d*/, "REWRITE_HERE" + i),
+    );
+  }
   console.log(i);
 }
